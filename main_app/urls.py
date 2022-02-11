39158ee7c72a53df0b52526dbc70d8fa71ef980c
Jason <jay.garriques@gmail.com>
2022-02-10 22:22:36 -0500

    seed data

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
-# add the base code to hold url requests.
+# define all routes
 urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+    path('about/', views.ArchitectList.as_view(), name="architects_list"),
 
 ]
