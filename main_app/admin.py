bab8dc716293a92fd55c0755e08039da469f97cb
Jason <jay.garriques@gmail.com>
2022-02-14 02:44:32 -0500

    2nd model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Architect # import the Artist model from models.py
+from .models import Architect, Building # import the Artist model from models.py
 # Register your models here.
 
 
 # Register your models here.
 
 admin.site.register(Architect) # this line will add the model to the admin panel
+admin.site.register(Building)
--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import time
+
 
 # Create your models here.
 class Architect(models.Model):
@@ -15,3 +17,16 @@ class Architect(models.Model):
 
         class Meta:
             ordering = ['name']
+
+
+class Building(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.CharField(max_length=300)
+    location = models.CharField(max_length=150)
+    architect = models.ForeignKey(Architect, on_delete=models.CASCADE, related_name="buildings")
+
+    def __str__(self):
+        return self.name
+    
+    def get_length(self):
+        return time.strftime("%-M:%S", time.gmtime(self.length))
